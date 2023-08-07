6fbcad867ffabf41052079d14c828a03cebdd16e
Ankush Singal <andysingal@gmail.com>
2023-08-07 14:07:35 +0530

    Add files via upload

---
+++ sdxl-projects1/sd-webui-animatediff-vungvk/README.md
@@ -0,0 +1,99 @@
+# AnimateDiff for Stable Diffusion Webui
+
+This extension aim for integrating [AnimateDiff](https://github.com/guoyww/AnimateDiff/) into [AUTOMATIC1111 Stable Diffusion WebUI](https://github.com/AUTOMATIC1111/stable-diffusion-webui). I have tested this extension with WebUI v1.4.1 on Ubuntu 20.04 with NVIDIA 3090. You can generate GIFs in exactly the same way as generating images after enabling this extension.
+
+This extension implements AnimateDiff in a different way. It does not require you to clone the whole SD1.5 repository. It also applied (probably) the least modification to `ldm`, so that you do not need to reload your model weights if you don't want to.
+
+Batch size on WebUI will be replaced by GIF frame number internally: 1 full GIF generated in 1 batch. If you want to generate multiple GIF at once, please change batch number. 
+
+Batch number is NOT the same as batch size. In A1111 WebUI, batch number is above batch size. Batch number means the number of sequential steps, but batch size means the number of parallel steps. You do not have to worry too much when you increase batch number, but you do need to worry about your VRAM when you increase your batch size (where in this extension, video frame number). You do not need to change batch size at all when you are using this extension.
+
+You might also be interested in another extension I created: [Segment Anything for Stable Diffusion WebUI](https://github.com/continue-revolution/sd-webui-segment-anything).
+
+## How to Use
+
+1. Install this extension via link.
+2. Download motion modules from [Google Drive](https://drive.google.com/drive/folders/1EqLC65eR1-W-sGD0Im7fkED6c8GkiNFI) | [HuggingFace](https://huggingface.co/guoyww/animatediff) | [CivitAI](https://civitai.com/models/108836) | [Baidu NetDisk](https://pan.baidu.com/s/18ZpcSM6poBqxWNHtnyMcxg?pwd=et8y). You only need to download one of `mm_sd_v14.ckpt` | `mm_sd_v15.ckpt`. Put the model weights under `sd-webui-animatediff/model/`. DO NOT change model filename.
+3. Go to txt2img if you want to try txt2gif and img2img if you want to try img2gif.
+4. Choose an SD1.5 checkpoint, write prompts, set configurations such as image width/height. If you want to generate multiple GIFs at once, please change batch number, instead of batch size.
+5. Enable AnimateDiff extension, set up each parameter (loop number means how many loop the GIF will be displayed, not the actual length of the GIF) and click `Generate`. 
+5. You should see the output GIF on the output gallery. You can access GIF output at `stable-diffusion-webui/outputs/{txt2img or img2img}-images/AnimateDiff`. You can also access image frames at `stable-diffusion-webui/outputs/{txt2img or img2img}-images/{date}`.
+
+## Update
+
+- `2023/07/20` [v1.1.0](https://github.com/continue-revolution/sd-webui-animatediff/releases/tag/v1.1.0): fix gif duration, add loop number, remove auto-download, remove xformers, remove instructions on gradio UI, refactor README, add [sponsor](#sponsor) QR code.
+- `2023/07/24` [v1.2.0](https://github.com/continue-revolution/sd-webui-animatediff/releases/tag/v1.2.0): fix incorrect insertion of motion modules, add option to change path to save motion modules in Settings/AnimateDiff, fix loading different motion modules.
+- `2023/07/27` [v1.2.1](https://github.com/continue-revolution/sd-webui-animatediff/releases/tag/v1.2.1): add hash calculation of motion modules (you can disable it in `Settings/AnimateDiff`) 
+
+## TODO
+This TODO list will most likely be resolved sequentially.
+- [ ] greyer sample
+- [ ] other attention optimization (e.g. sdp)
+- [ ] img2img
+- [ ] [token](https://github.com/continue-revolution/sd-webui-animatediff/issues/4)
+- [ ] [shape](https://github.com/continue-revolution/sd-webui-animatediff/issues/3)
+- [ ] [reddit](https://www.reddit.com/r/StableDiffusion/comments/152n2cr/a1111_extension_of_animatediff_is_available/?sort=new)
+
+## FAQ
+1.  Q: I am using a remote server which blocks Google. What should I do?
+
+    A: You will have to find a way to download motion modules locally and re-upload to your server.
+
+2.  Q: How much VRAM do I need?
+
+    A: Currently, you can run WebUI with this extension via NVIDIA 3090. I cannot guarantee any other variations of GPU. Actual VRAM usage depends on your image size and video frame number. You can try to reduce image size or video frame number to reduce VRAM usage. The default setting (displayed in [Samples/txt2img](#txt2img) section) consumes 12GB VRAM. More VRAM info will be added later.
+
+3.  Q: Can I generate a video instead a GIF?
+
+    A: Unfortunately, you cannot. This is because a whole batch of images will pass through a transformer module, which prevents us from generating videos sequentially. We look forward to future developments of deep learning for video generation.
+
+4.  Q: Can I use SDXL to generate GIFs?
+
+    A: At least at this time, you cannot. This extension essentially inject multiple motion modules into SD1.5 UNet. It does not work for other variations of SD, such as SD2.1 and SDXL. I'm not sure what will happen if you force-add motion modules to SD2.1 or SDXL. Future experiments are needed.
+
+5.  Q: Can I use this extension to do gif2gif?
+
+    A: Due to the 1-batch behavior of AnimateDiff, it is probably not possible to support gif2gif. However, I need to discuss this with the authors of AnimateDiff.
+
+6.  Q: Can I use xformers?
+
+    A: Yes, but it will not be applied to AnimateDiff due to [a weird bug](https://github.com/continue-revolution/sd-webui-animatediff/issues/2). I will try other optimizations. Note that xformers will change the GIF you generate.
+
+7.  Q: This extension perform worse than [AnimateDiff](https://github.com/guoyww/AnimateDiff/). There seem to be no motion but only glitches. Why?
+
+    A: Because I inserted motion modules to the wrong place inside UNet output blocks. It is a very idiot typo (I wrote a 2 where it was supposed to be 3) but took me days to discover.
+
+8.  Q: How can I reproduce the result in [Samples/txt2img](#txt2img) section?
+
+    A: You must replace [create_random_tensors](https://github.com/AUTOMATIC1111/stable-diffusion-webui/blob/master/modules/processing.py#L479-L537) with 
+    ```python
+        torch.manual_seed(<seed>)
+        from einops import rearrange
+        x = rearrange(torch.randn((4, 16, 64, 64), device=shared.device), 'c f h w -> f c h w')
+    ```
+    and retry. A1111 generate random tensors in a completely different way.
+
+9. Q: [v1.2.0](https://github.com/continue-revolution/sd-webui-animatediff/releases/tag/v1.2.0) does not work for img2img. Why?
+
+    A: I don't know. I will try to figure out why very soon.
+
+10. Q: [v1.2.0](https://github.com/continue-revolution/sd-webui-animatediff/releases/tag/v1.2.0) seems to give a greyer sample compared to [AnimateDiff](https://github.com/guoyww/AnimateDiff/). Why?
+
+    A: I don't know. I will try to figure out why very soon.
+
+## Samples
+
+### txt2img
+| AnimateDiff | A1111 |
+| --- | --- |
+| ![image](https://user-images.githubusercontent.com/63914308/255306527-5105afe8-d497-4ab1-b5c4-37540e9601f8.gif) | ![00023-10788741199826055168](https://github.com/continue-revolution/sd-webui-animatediff/assets/63914308/c35a952a-a127-491b-876d-cda97771f7ee) |
+
+### img2img
+[v1.2.0](https://github.com/continue-revolution/sd-webui-animatediff/releases/tag/v1.2.0) does not work for img2img due to some unknown reason. Will be fixed later.
+
+## Sponsor
+You can sponsor me via WeChat or Alipay.
+
+| WeChat | Alipay |
+| --- | --- |
+| ![216aff0250c7fd2bb32eeb4f7aae623](https://user-images.githubusercontent.com/63914308/232824466-21051be9-76ce-4862-bb0d-a431c186fce1.jpg) | ![15fe95b4ada738acf3e44c1d45a1805](https://user-images.githubusercontent.com/63914308/232824545-fb108600-729d-4204-8bec-4fd5cc8a14ec.jpg) |
---
+++ sdxl-projects1/sd-webui-animatediff-vungvk/scripts/animatediff.py
@@ -0,0 +1,163 @@
+import os
+import gc
+import gradio as gr
+import imageio
+import torch
+from einops import rearrange
+
+from modules import scripts, images, shared, script_callbacks, hashes
+from modules.devices import torch_gc, device, cpu
+from modules.processing import StableDiffusionProcessing, Processed
+from scripts.logging_animatediff import logger_animatediff
+from motion_module import MotionWrapper, VanillaTemporalModule
+
+
+from ldm.modules.diffusionmodules.openaimodel import TimestepBlock, TimestepEmbedSequential
+from ldm.modules.diffusionmodules.util import GroupNorm32
+from ldm.modules.attention import SpatialTransformer
+def mm_tes_forward(self, x, emb, context=None):
+    for layer in self:
+        if isinstance(layer, TimestepBlock):
+            x = layer(x, emb)
+        elif isinstance(layer, (SpatialTransformer, VanillaTemporalModule)):
+            x = layer(x, context)
+        else:
+            x = layer(x)
+    return x
+TimestepEmbedSequential.forward = mm_tes_forward
+script_dir = scripts.basedir()
+groupnorm32_original_forward = GroupNorm32.forward
+
+
+class AnimateDiffScript(scripts.Script):
+    motion_module: MotionWrapper = None
+    
+    def __init__(self):
+        self.logger = logger_animatediff
+
+    def title(self):
+        return "AnimateDiff"
+    
+    def show(self, is_img2img):
+        return scripts.AlwaysVisible
+    
+    def unload_motion_module(self):
+        self.logger.info("Moving motion module to CPU")
+        if AnimateDiffScript.motion_module is not None:
+            AnimateDiffScript.motion_module.to(cpu)
+        torch_gc()
+        gc.collect()
+
+    def remove_motion_module(self):
+        self.logger.info("Removing motion module from any memory")
+        del AnimateDiffScript.motion_module
+        AnimateDiffScript.motion_module = None
+        torch_gc()
+        gc.collect()
+    
+    def ui(self, is_img2img):
+        with gr.Accordion('AnimateDiff', open=False):
+            model = gr.Dropdown(choices=["mm_sd_v15.ckpt", "mm_sd_v14.ckpt"], value="mm_sd_v15.ckpt", label="Motion module", type="value")
+            with gr.Row():
+                enable = gr.Checkbox(value=False, label='Enable AnimateDiff')
+                video_length = gr.Slider(minimum=1, maximum=24, value=16, step=1, label="Number of frames", precision=0)
+                fps = gr.Number(value=8, label="Frames per second (FPS)", precision=0)
+                loop_number = gr.Number(minimum=0, value=0, label="Display loop number (0 = infinite loop)", precision=0)
+            with gr.Row():
+                unload = gr.Button(value="Move motion module to CPU (default if lowvram)")
+                remove = gr.Button(value="Remove motion module from any memory")
+                unload.click(fn=self.unload_motion_module)
+                remove.click(fn=self.remove_motion_module)
+        return enable, loop_number, video_length, fps, model
+    
+    def inject_motion_modules(self, p: StableDiffusionProcessing, model_name="mm_sd_v15.ckpt"):
+        model_path = "/content/drive/MyDrive/StableDiffusion/Model/" + model_name
+        if not os.path.isfile(model_path):
+            raise RuntimeError("Please download models manually.")
+        if AnimateDiffScript.motion_module is None or AnimateDiffScript.motion_module.mm_type != model_name:
+            if shared.opts.data.get("animatediff_check_hash", True):
+                def get_mm_hash(model_name="mm_sd_v15.ckpt"):
+                    if model_name == "mm_sd_v14.ckpt":
+                        return 'aa7fd8a200a89031edd84487e2a757c5315460eca528fa70d4b3885c399bffd5'
+                    elif model_name == "mm_sd_v15.ckpt":
+                        return 'cf16ea656cb16124990c8e2c70a29c793f9841f3a2223073fac8bd89ebd9b69a'
+                    else:
+                        raise RuntimeError(f"Unsupported model filename {model_name}. Should be one of mm_sd_v14.ckpt or mm_sd_v15.ckpt")
+                if hashes.sha256(model_path, f"AnimateDiff/{model_name}") != get_mm_hash(model_name):
+                    raise RuntimeError(f"{model_name} hash mismatch. You probably need to re-download the motion module.")
+            self.logger.info(f"Loading motion module {model_name} from {model_path}")
+            mm_state_dict = torch.load(model_path, map_location=device)
+            AnimateDiffScript.motion_module = MotionWrapper(model_name)
+            missed_keys = AnimateDiffScript.motion_module.load_state_dict(mm_state_dict)
+            self.logger.warn(f"Missing keys {missed_keys}")
+        AnimateDiffScript.motion_module.to(device)
+        unet = p.sd_model.model.diffusion_model
+        self.logger.info(f"Hacking GroupNorm32 forward function.")
+        def groupnorm32_mm_forward(self, x):
+            x = rearrange(x, '(b f) c h w -> b c f h w', b=2)
+            x = groupnorm32_original_forward(self, x)
+            x = rearrange(x, 'b c f h w -> (b f) c h w', b=2)
+            return x
+        GroupNorm32.forward = groupnorm32_mm_forward
+        self.logger.info(f"Injecting motion module {model_name} into SD1.5 UNet input blocks.")
+        for mm_idx, unet_idx in enumerate([1, 2, 4, 5, 7, 8, 10, 11]):
+            mm_idx0, mm_idx1 = mm_idx // 2, mm_idx % 2
+            unet.input_blocks[unet_idx].append(AnimateDiffScript.motion_module.down_blocks[mm_idx0].motion_modules[mm_idx1])
+        self.logger.info(f"Injecting motion module {model_name} into SD1.5 UNet output blocks.")
+        for unet_idx in range(12):
+            mm_idx0, mm_idx1 = unet_idx // 3, unet_idx % 3
+            if unet_idx % 3 == 2 and unet_idx != 11:
+                unet.output_blocks[unet_idx].insert(-1, AnimateDiffScript.motion_module.up_blocks[mm_idx0].motion_modules[mm_idx1])
+            else:
+                unet.output_blocks[unet_idx].append(AnimateDiffScript.motion_module.up_blocks[mm_idx0].motion_modules[mm_idx1])
+        self.logger.info(f"Injection finished.")
+
+
+    def remove_motion_modules(self, p: StableDiffusionProcessing):
+        unet = p.sd_model.model.diffusion_model
+        self.logger.info(f"Removing motion module from SD1.5 UNet input blocks.")
+        for unet_idx in [1, 2, 4, 5, 7, 8, 10, 11]:
+            unet.input_blocks[unet_idx].pop(-1)
+        self.logger.info(f"Removing motion module from SD1.5 UNet output blocks.")
+        for unet_idx in range(12):
+            if unet_idx % 3 == 2 and unet_idx != 11:
+                unet.output_blocks[unet_idx].pop(-2)
+            else:
+                unet.output_blocks[unet_idx].pop(-1)
+        self.logger.info(f"Restoring GroupNorm32 forward function.")
+        GroupNorm32.forward = groupnorm32_original_forward
+        self.logger.info(f"Removal finished.")
+        if shared.cmd_opts.lowvram:
+            self.unload_motion_module()
+    
+    def before_process(self, p: StableDiffusionProcessing, enable_animatediff=False, loop_number=0, video_length=16, fps=8, model="mm_sd_v15.ckpt"):
+        if enable_animatediff:
+            self.logger.info(f"AnimateDiff process start with video Max frames {video_length}, FPS {fps}, duration {video_length/fps},  motion module {model}.")
+            assert video_length > 0 and fps > 0, "Video length and FPS should be positive."
+            p.batch_size = video_length
+            self.inject_motion_modules(p, model)
+    
+    def postprocess(self, p: StableDiffusionProcessing, res: Processed, enable_animatediff=False, loop_number=0, video_length=16, fps=8, model="mm_sd_v15.ckpt"):
+        if enable_animatediff:
+            self.remove_motion_modules(p)
+            video_paths = []
+            self.logger.info("Merging images into GIF.")
+            from pathlib import Path
+            Path(f"{p.outpath_samples}/AnimateDiff").mkdir(exist_ok=True)
+            for i in range(res.index_of_first_image, len(res.images), video_length):
+                video_list = res.images[i:i+video_length]
+                seq = images.get_next_sequence_number(f"{p.outpath_samples}/AnimateDiff", "")
+                filename = f"{seq:05}-{res.seed}"
+                video_path = f"{p.outpath_samples}/AnimateDiff/{filename}.gif"
+                video_paths.append(video_path)
+                imageio.mimsave(video_path, video_list, duration=(video_length/fps), loop=loop_number)
+            res.images = video_paths
+            self.logger.info("AnimateDiff process end.")
+
+def on_ui_settings():
+    section = ('animatediff', "AnimateDiff")
+    shared.opts.add_option("animatediff_model_path", shared.OptionInfo(os.path.join(script_dir, "model"), "Path to save AnimateDiff motion modules", gr.Textbox, section=section))
+    shared.opts.add_option("animatediff_check_hash", shared.OptionInfo(True, "Check hash for motion modules. Disable checking if you want to use your own.", gr.Checkbox, section=section))
+
+
+script_callbacks.on_ui_settings(on_ui_settings)
